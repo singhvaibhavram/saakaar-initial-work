e944014d6adf9cfadbfb7ad7906edd0cb5fbb766
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2020-01-08 23:11:23 +0530

    Register API

--- src/pages/Eventregister.js
+++ src/pages/Eventregister.js
@@ -4,7 +4,7 @@ import '../assets/css/eventform.css'
 class Eventregister extends React.Component{
 
     async componentDidMount() {
-        const url = "http://49d3e244.ngrok.io/v0/city"
+        const url = " http://4950d5b1.ngrok.io/v0/city"
         const response = await fetch(url)
         const cityData = await response.json()
         this.setState({cities: cityData})
@@ -17,47 +17,59 @@ class Eventregister extends React.Component{
         cities: [],
         age: "",
         kids: "",
+        cityId: "",
     }
 
     validate = () =>{
         if(this.state.phnumber.length !== 10){
             alert("Phone Number should be 10 digits")
+            return false;
         }
 
         if(!this.state.fname){
             alert("First Name Cannot be blank")
+            return false;
         }
 
         if(!this.state.lname){
             alert("Last Name Cannot be blank")
+            return false;
         }
 
 
 
         if(!this.state.age){
             alert("Age Cannot be blank")
+            return false;
         }
 
         if(!this.state.kids){
             alert("Kids Cannot be blank")
+            return false;
         }
+
+        return true;
     }
 
     handleChange = (event, fieldName) => {
         console.log(fieldName)
         this.setState({[fieldName]: event.target.value})
+        console.log(event)
     }
-    handleSubmit = () => {
+
+    handleSubmit = (e) => {
         const isValid = this.validate()
         if(isValid) {
+            e.preventDefault();
             var data = JSON.stringify({
                               "first_name": this.state.fname,
                               "last_name": this.state.lname,
                               "mobile_number": this.state.phnumber,
-                              "city_id": this.state.cityId,
+                              "city_id": "1",
                               "age": this.state.age,
                               "kids": this.state.kids
                             });
+            console.log(data)
             var xhr = new XMLHttpRequest();
             xhr.withCredentials = true;
             xhr.addEventListener("readystatechange", function () {
@@ -65,7 +77,7 @@ class Eventregister extends React.Component{
                 alert(this.responseText);
               }
             });
-            xhr.open("POST", "http://45eadb93.ngrok.io/v0/register");
+            xhr.open("POST", " http://4950d5b1.ngrok.io/v0/register");
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.setRequestHeader("Host", "45eadb93.ngrok.io");
             xhr.send(data);
@@ -85,7 +97,7 @@ class Eventregister extends React.Component{
                                     <input type="text" name={"first_name"} value={this.state.fname} onChange={(event) => this.handleChange(event, "fname")} placeholder={"First Name"}/>
                                 </td>
                                 <td>
-                                    <input type="text" name={"last_name"} value={this.state.lname} onChange={(event) => this.handleChange(event, "lname")} placeholder={"Last Name"}/>
+                                    <input type="text" className={"LastName"} name={"last_name"} value={this.state.lname} onChange={(event) => this.handleChange(event, "lname")} placeholder={"Last Name"}/>
                                 </td>
                             </tr>
                             <tr>
@@ -95,15 +107,15 @@ class Eventregister extends React.Component{
                             </tr>
                             <tr>
                                 <td>
-                                    <input value={this.state.cityId} type="text" name={"city_id"} list="cityList" placeholder={"City"}/>
+                                    <input type="text" name={"city_id"} list="cityList" placeholder={"City"}/>
                                     <datalist id="cityList">
                                         {this.state.cities.map(city => (
-                                            <option>{city.english}  {city.hindi}</option>
+                                            <option>{city.english} {city.hindi}</option>
                                         ))}
                                     </datalist>
                                 </td>
                                 <td>
-                                    <input type="number" name={"age"} value={this.state.age} onChange={(event) => this.handleChange(event, "age")} placeholder={"Age"}/>
+                                    <input type="number" name={"age"} className={"Age"} value={this.state.age} onChange={(event) => this.handleChange(event, "age")} placeholder={"Age"}/>
                                 </td>
                             </tr>
                             <tr>
